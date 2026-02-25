8bcc47735836e2fb258b9f479ffe7cbc42bfa680
Botaro <114265708+r4f1nha@users.noreply.github.com>
2026-02-25 19:42:02 -0300

    adicionando novos updates (#3)
    Adicionando Classe Service.

--- back/src/main/java/com/botaro/portifolio/service/ProjectService.java
+++ back/src/main/java/com/botaro/portifolio/service/ProjectService.java
@@ -1,61 +1,50 @@
-package com.botaro.portifolio.api;
+package com.botaro.portifolio.service;
 
 import com.botaro.portifolio.api.dto.ProjectRequest;
 import com.botaro.portifolio.api.dto.ProjectResponse;
 import com.botaro.portifolio.domain.Project;
-import org.springframework.web.bind.annotation.*;
 import com.botaro.portifolio.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/projects")
-public class ProjectController {
+@Service
+public class ProjectService {
 
-    private final ProjectRepository projectRepository;
+    private final ProjectRepository   projectRepository;
 
-    public ProjectController(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository){
         this.projectRepository = projectRepository;
     }
 
-    @GetMapping("/getAllProjects")
     public List<ProjectResponse> list(){
         return projectRepository.findAll().stream()
                 .map(p -> new ProjectResponse(p.getId(), p.getTitle(), p.getDescription()))
                 .toList();
     }
 
-    @GetMapping("/{id}")
-    public Optional<Project> getProjectById(@PathVariable  long id){
+    public Optional<Project> getProjectById(@PathVariable long id) {
         return projectRepository.findById(id);
     }
-
-    @PostMapping("/create")
     public Project create(@RequestBody ProjectRequest request){
-            Project project = new Project(
-                    request.title(),
-                    request.description());
-            return projectRepository.save(project);
+        Project project = new Project(
+                request.title(),
+                request.description());
+        return projectRepository.save(project);
     }
-    @PutMapping("/update")
     public Project update(@RequestBody ProjectResponse request){
         Project project = projectRepository.findById(request.id())
                 .orElseThrow(() -> new RuntimeException("Id nao encontrado" + request.id()));
 
-           project.setTitle(request.title());
-           project.setDescription(request.description());
+        project.setTitle(request.title());
+        project.setDescription(request.description());
 
         return projectRepository.save(project);
     }
-
-    @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable long id){
-        try {
-            projectRepository.deleteById(id);
-            System.out.println("Projeto deletado com sucesso.");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        projectRepository.deleteById(id);
     }
 }
